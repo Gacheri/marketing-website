e98c9c728cc1284063a55a598377641bcff8cb8c
Gacheri <michenibrenda81@gmail.com>
2021-08-13 22:09:26 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from "React";
+
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                <h1>Jambo.</h1>
+
+            </div>
+        );
+    }
+}
+
+export default App;
+// export default function App(){
+//     return <h1>Jambo Kenya!!!</h1>
+// }
